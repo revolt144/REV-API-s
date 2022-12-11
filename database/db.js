6e8650ab123a2ff3b88497c73581e40e12116dc5
revolt144 <74239459+revolt144@users.noreply.github.com>
2022-12-11 13:33:10 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,16 +3,16 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://api.sekha.me';
+var url = 'mongodb+srv://Xontol:Namakuhacker88@test.0fghsfu.mongodb.net/?retryWrites=true&w=majority';
 try {
-if(url == 'https://api.sekha.me') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'mongodb+srv://Xontol:Namakuhacker88@test.0fghsfu.mongodb.net/?retryWrites=true&w=majority') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
 var db = monk(url);
 
 db.then(() => {
-  console.log(color('Connected correctly to server, oke','green'))
+  console.log(color('Connected correctly to server, Revolt','green'))
 })
 .catch ((e) => {
 	console.log(color('Error : '+ e +'\n\nGagal connect ke database, \ncek configurasi database apakah Connection URL sudah benar','red'))
